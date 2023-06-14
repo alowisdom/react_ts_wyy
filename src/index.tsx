c1606c3fcfff5e361516355913832f34be44060a
alowisdom <alowisdom@163.com>
2023-06-14 12:59:17 +0800

    存档

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,10 @@ import ReactDOM from "react-dom/client"
 import "normalize.css"
 import "@/assets/css/index.less"
 import { BrowserRouter } from "react-router-dom"
+import { Provider } from "react-redux"
 
 import App from "@/App"
+import store from "./store"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 
@@ -12,7 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
     /* 懒加载没下载完 js 文件之前，要包裹一层suspense */
 }
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 )
